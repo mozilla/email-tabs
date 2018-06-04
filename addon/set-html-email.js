650c8b7095a206a151f4c72b982929301ad17dc2
Ian Bicking <ianb@colorstudy.com>
2018-06-04 15:28:32 -0500

    Fix #30, make the images in the email actually work
    This triggers a fake paste to gmail, which makes gmail fix up any images that need fixing up. This involves uploading the images to gmail, so unfortunately it's a bit pokey

--- addon/set-html-email.js
+++ addon/set-html-email.js
@@ -9,4 +9,9 @@ function setHtml(html) {
     return;
   }
   editableEl.innerHTML = html + "\n<br />" + editableEl.innerHTML; // eslint-disable-line no-unsanitized/property
+  // Gmail does a fixup on paste, so we have to simulate a paste to make it fix the images we inserted:
+  let paste = new Event("paste");
+  paste = paste.wrappedJSObject;
+  paste.clipboardData = cloneInto({getData: function() {}}, window, {cloneFunctions: true});
+  editableEl.dispatchEvent(paste);
 }
