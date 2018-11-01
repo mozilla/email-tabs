6aa19295dbb95425cfbd73fdaac823299f98750b
Dave Justice <djustice@mozilla.com>
2018-11-01 19:45:09 +0000

    Fix lint errors (#213)
    * Fix lint errors
    * fix alt attrs

--- addon/options/options.js
+++ addon/options/options.js
@@ -1,14 +1,14 @@
 function saveOptions(e) {
   browser.storage.local.set({
-    showAdditionalProviders: document.querySelector("#provider").checked
+    showAdditionalProviders: document.querySelector("#provider").checked,
   });
   e.preventDefault();
 }
 
 async function restoreOptions() {
-  const showAdditionalProviders = await browser.storage.local.get('showAdditionalProviders');
+  const showAdditionalProviders = await browser.storage.local.get("showAdditionalProviders");
   document.querySelector("#provider").checked = showAdditionalProviders;
 }
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
 document.querySelector("form").addEventListener("submit", saveOptions);
